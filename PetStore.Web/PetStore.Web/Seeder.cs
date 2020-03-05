9c474334f69358f1749b73e21e711ca8a8320ea9
duchovny1 <54773747+duchovny1@users.noreply.github.com>
2020-03-05 22:38:57 +0200

    small changes

---
+++ PetStore.Web/PetStore.Web/Seeder.cs
@@ -0,0 +1,30 @@
+﻿using PetStore.Data;
+using PetStore.Data.Models;
+using System.Threading.Tasks;
+
+namespace PetStore.Web
+{
+    public class Seeder
+    {
+        private readonly PetStoreDbContext db;
+
+        public Seeder(PetStoreDbContext db)
+        {
+            this.db = db;
+            this.db.Database.EnsureCreated();
+        }
+        internal async Task SeedAsync()
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                Breed breed = new Breed();
+
+                breed.Name = $"Breed {i}";
+
+                await this.db.AddAsync(breed);
+            }
+
+            this.db.SaveChanges();
+        }
+    }
+}
